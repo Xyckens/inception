9fe9118e606d17bd2599a9d29a2c356bdbd1fc29
fvieira <franciscommvieira@hotmail.com>
2023-06-13 16:00:30 +0100

    wp config done, need to work on the env var

--- srcs/requirements/wordpress/tools/script.sh
+++ srcs/requirements/wordpress/tools/script.sh
@@ -1,4 +1,14 @@
 #!bin/bash
 
 cp -f /tmp/wp-config.php /var/www/html
-chmod 644 /var/www/html/wp-config.php;;
+chmod 644 /var/www/html/wp-config.php;
+
+# https://developer.wordpress.org/cli/commands/core/install/
+wp core install --url="fvieira.42.fr --admin_name"evaluator" --admin_password="1234" --allow_root
+
+wp user create "franc" --user_pass="1234" --allow-root
+
+service php7.3-fpm start;
+service php7.3-fpm stop;
+
+php-fpm7.3 -F -R
